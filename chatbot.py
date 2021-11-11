1ac7efbeeaaacc21538c5a0fe0877aa64d74e6c3
Avinash Raj <86678366+MalakaAvinashRaj@users.noreply.github.com>
2021-11-11 23:40:48 +0530

    Add files via upload

--- chatbot.py
+++ chatbot.py
@@ -5,13 +5,18 @@ from data_base import search_process
 # Naming bot
 bot_name = 'sara:'
 
+response = {'hi', 'hello', 'hola'}
+
 # Chat starts here
 def chatting(get_response):
     
-    if 'hi' in get_response:
-        return 'hello'
+    if get_response in response:
+        return 'Hello...  Type your Error below'
     
     elif 'end' in get_response:
+        return "terminate"
+
+    elif 'exit' in get_response:
         return "terminate"
     
     else:
